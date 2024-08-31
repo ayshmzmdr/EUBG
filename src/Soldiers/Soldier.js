8265f14f6d214fb4fc51fa459071c8033113acff
RishavMz <rishav.mazumdar123@gmail.com>
2024-08-31 15:14:21 +0530

    Added basic folder structure

---
+++ src/Soldiers/Soldier.js
@@ -0,0 +1,21 @@
+class Soldier {
+    constructor() {
+        this.position = 0
+        this.attack = 0.0
+        this.hitpoints = 0.0
+        this.range = 0
+        this.attack_cooldown = 0.0
+        this.speed = 0
+        this.priority = 0
+        this.alive = false
+        this.moving = false
+    }
+
+    takeDamage() {}
+
+    animateMove() {}
+
+    display() {}
+
+    animateDamage() {}
+}
