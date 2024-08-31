4edf80016de52ee85a9a80a118414c48079cca16
Ayush Mazumdar <ayush.mazumdar111@gmail.com>
2024-08-31 16:27:04 +0530

    Added Player1 and Player2 Home

--- test/board.js
+++ test/board.js
@@ -10,9 +10,18 @@ for(let i=0;i<9;i++){
 const cardName=["Giant","Archer","Zombie"]
 const card=[];
 
+const pl1=document.createElement("button")
+pl1.classList.add("pl1");
+document.body.prepend(pl1);
+
 for(let i=0;i<cardName.length;i++){
     card[i]=document.createElement("button");
+    card[i].disabled;
     card[i].id=`card${i}`;
     card[i].classList.add("card");
     document.body.prepend(card[i]);
 }
+
+const pl2=document.createElement("button")
+pl2.classList.add("pl2");
+document.body.prepend(pl2);
