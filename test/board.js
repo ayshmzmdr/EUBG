894b1fb2c4f8cc2f3786a199b9d1944f0aa75c00
Ayush Mazumdar <ayush.mazumdar111@gmail.com>
2024-08-31 17:13:15 +0530

    Added shadow texture

--- test/board.js
+++ test/board.js
@@ -1,27 +1,28 @@
-const boxID=[];
+const boxID = [];
 
-for(let i=0;i<9;i++){
-    boxID[i]=document.createElement("button");
-    boxID[i].id=`box${i}`;
-    boxID[i].classList.add("box");
-    document.body.prepend(boxID[i]);
+for (let i = 0; i < 9; i++) {
+  boxID[i] = document.createElement("button");
+  boxID[i].id = `box${i}`;
+  boxID[i].classList.add("box");
+  document.body.prepend(boxID[i]);
 }
+boxID[4].classList.add("middleBox");
 
-const cardName=["Giant","Archer","Zombie"]
-const card=[];
+const cardName = ["Giant", "Archer", "Zombie"];
+const card = [];
 
-const pl1=document.createElement("button")
+const pl1 = document.createElement("button");
 pl1.classList.add("pl1");
 document.body.prepend(pl1);
 
-for(let i=0;i<cardName.length;i++){
-    card[i]=document.createElement("button");
-    card[i].disabled;
-    card[i].id=`card${i}`;
-    card[i].classList.add("card");
-    document.body.prepend(card[i]);
+for (let i = 0; i < cardName.length; i++) {
+  card[i] = document.createElement("button");
+  card[i].disabled;
+  card[i].id = `card${i}`;
+  card[i].classList.add("card");
+  document.body.prepend(card[i]);
 }
 
-const pl2=document.createElement("button")
+const pl2 = document.createElement("button");
 pl2.classList.add("pl2");
 document.body.prepend(pl2);
