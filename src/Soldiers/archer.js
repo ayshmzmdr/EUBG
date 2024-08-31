44dec98ce613cd66593af50553894c13b917d0b5
Ayush Mazumdar <ayush.mazumdar111@gmail.com>
2024-08-31 21:27:27 +0530

    Added Character data

--- src/Soldiers/archer.js
+++ src/Soldiers/archer.js
@@ -1,15 +1,15 @@
 import Soldier from "./soldier.js";
 
-class Giant extends Soldier{
+class Archer extends Soldier{
   constructor(id, posx, posy) {
     super(id,posx,posy);
     this.position = 0;
-    this.range = 1;
-    this.speed = 2;
-    this.priority = 2;
-    this.attack = 8.0;
-    this.hitpoints = 25.0;
-    this.attack_cooldown = 1.0;
+    this.range = 3;
+    this.speed = 5;
+    this.priority = 0;
+    this.attack = 5.0;
+    this.hitpoints = 15.0;
+    this.attack_cooldown = 0.75;
     this.alive = true;
     this.moving = false;
     this.friendly = false;
@@ -28,4 +28,4 @@ class Giant extends Soldier{
   }*/
 }
 
-export default Giant;
+export default Archer;
